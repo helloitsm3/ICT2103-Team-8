24ae2836915a85072ae5e3a71728a8937d2dcad1
ccaian <ccaian3@gmail.com>
2020-09-19 20:55:29 +0800

    added my part
    scrapper\movie_data_lib.py
    scrapper\scarpper_lib.py
    
    these two libraries are used to fetch movie data and cinema showing movies
    
    also added test code to the main, that just prints out to console

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+import scrapper.movie_data_lib as movie_lib
+import scrapper.scrapper_lib as scrapper_lib
 # ===== Routing =====
 from routes.main_routes import data
 
@@ -7,3 +8,17 @@ app = Flask(__name__)
 
 # ===== Blueprints Registration =====
 app.register_blueprint(data, url_prefix="/")
+
+
+#TESTING FUNCTIONS
+movielist = scrapper_lib.cathay_scraper()
+print("★★★Now Showing★★★")
+for i in movielist:
+
+    movie = movie_lib.tmdb_fetch(i)
+    if movie:
+        print("Movie Name: ",movie[0]['title'])
+        print("Release Date: ",movie[0]['release_date'])
+        print("Rating: ",movie[0]['vote_average'])
+        print("Description: ",movie[0]['overview'])
+        print("=========================================")
---
+++ scrapper/scrapper_lib.py
@@ -0,0 +1,52 @@
+'''
+
+Web Scrapping Library
+General purpose library for any web scraping functions
+
+'''
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+#list of urls to be scrapped
+cathayUrl = 'https://www.cathaycineplexes.com.sg/movies/'
+
+
+'''
+Function to replace a list of strings in a string
+'''
+def multiple_replace(dict, text):
+  regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+
+
+'''
+Function to scrape the cathay movie website
+Returns a list of movie names in a 2d array, no data.
+'''
+def cathay_scraper():
+
+    movieList = []
+    # list of all ratings, to remove from the scrapped movie title
+    ageTag = {'NC16':'','PG13':'','M18':'','TBA':'','PG':'','(Mandarin)':''}
+
+    #download the page using bs4
+    page = requests.get(cathayUrl)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #counter to help skip every other iteration, because of duplicates
+    counter = 0
+
+    #select all the movie titles, which are under the h3 tags
+    for t in soup.find_all('h3', text=True):
+        if counter == 1:
+            counter = 0
+            continue
+        else:
+            counter = 1
+        #remove unneccsaryy characters with re
+        title = re.sub('[*]','',t.text)
+        movieList.append(multiple_replace(ageTag,title))
+
+    return movieList
