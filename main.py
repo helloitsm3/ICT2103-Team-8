896d2d5e2f754059f9b74c41423476178406fe26
helloitsme <seanlengws@gmail.com>
2020-10-24 00:04:52 +0800

    Made some minor changes

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 # ===== Blueprints Registration =====
 app.register_blueprint(data, url_prefix="/")
 
-data = Database(database="mongo")
-# data.initMySQLTable()
+data = Database(database="mysql")
+data.getMySQLCursor()
 
 """
 #TESTING FUNCTIONS
