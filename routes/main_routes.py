998f5fc93351d4dab1c89252a42764aaaabea4d1
helloitsme <seanlengws@gmail.com>
2020-10-31 20:37:59 +0800

    Made some minor changes

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -5,9 +5,10 @@ import scripts.scrapper.movie_controller as movie_controller
 data = Blueprint("main_api", __name__)
 
 
-@data.route("/home")
-def getHome():
-    return '{"Data": "Welcome to the homepage"}'
+@data.route("/")
+def main():
+    images = movie_controller.getCathayMainPosters()
+    return render_template("main.html", images=images)
 
 
 @data.route("/nowshowing")
@@ -21,12 +22,6 @@ def getShowing():
 def getMovieDetail():
     poster = request.form["posterurl[]"]
     return render_template("moviedetail.html", poster=poster)
-
-
-@data.route("/")
-def main():
-    images = movie_controller.getCathayMainPosters()
-    return render_template("main.html", images=images)
 
 
 @data.route("/nowshowing/<moviename>", methods=["GET", "POST"])
